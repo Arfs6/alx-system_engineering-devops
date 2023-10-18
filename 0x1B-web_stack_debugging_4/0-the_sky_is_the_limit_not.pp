1bb0c7b6bab8ab58f27afc978a8ee528ecc2c59b
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-10-18 21:38:19 +0100

    fixing server...

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Fix server.
+
+exec { 'sed-edit-config':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => "/usr/local/bin/:/bin"
+}
+exec { 'sudo-restart-nginx':
+  command => "sudo service nginx restart",
+  path    => "/usr/bin/:/bin/:/usr/local/bin/"
+}

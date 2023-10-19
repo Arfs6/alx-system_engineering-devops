588d350ab0d5d4f1df502f3691e2b2bf4c12f71e
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-10-19 17:39:01 +0100

    fix linting errors

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,9 +2,9 @@
 
 exec { 'sed-edit-config':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => "/usr/local/bin/:/bin"
+  path    => '/usr/local/bin/:/bin'
 }
 exec { 'sudo-restart-nginx':
-  command => "/etc/init.d/nginx restart",
-  path    => "/usr/bin/:/bin/:/usr/local/bin/"
+  command => '/etc/init.d/nginx restart',
+  path    => '/usr/bin/:/bin/:/usr/local/bin/'
 }
